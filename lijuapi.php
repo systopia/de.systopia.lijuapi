9d107c27b9ba7692cd17b05728f37280ccec1b5b
Philipp Batroff <batroff@systopia.de>
2022-05-31 18:21:29 +0200

    civix upgrade

--- lijuapi.php
+++ lijuapi.php
@@ -27,15 +27,6 @@ function lijuapi_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function lijuapi_civicrm_xmlMenu(&$files) {
-  _lijuapi_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -90,50 +81,6 @@ function lijuapi_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function lijuapi_civicrm_managed(&$entities) {
-  _lijuapi_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Add CiviCase types provided by this extension.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function lijuapi_civicrm_caseTypes(&$caseTypes) {
-  _lijuapi_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Add Angular modules provided by this extension.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function lijuapi_civicrm_angularModules(&$angularModules) {
-  // Auto-add module files from ./ang/*.ang.php
-  _lijuapi_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function lijuapi_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _lijuapi_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
@@ -143,14 +90,6 @@ function lijuapi_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
 function lijuapi_civicrm_entityTypes(&$entityTypes) {
   _lijuapi_civix_civicrm_entityTypes($entityTypes);
 }
-
-/**
- * Implements hook_civicrm_themes().
- */
-function lijuapi_civicrm_themes(&$themes) {
-  _lijuapi_civix_civicrm_themes($themes);
-}
-
 
 function lijuapi_civicrm_post($op, $objectName, $objectId, &$objectRef) {
   // only react for Email Entity update and edit
